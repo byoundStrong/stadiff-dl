0790de637822ef4bbeafb239dad79411809157d7
YeaGram <managerdims@gmail.com>
2023-03-22 07:22:56 +0700

    add validating

--- index.mjs
+++ index.mjs
@@ -1,20 +1,19 @@
-import expres, { json, text, urlencoded } from 'express'
+import expres from 'express'
 const app = expres();
 import cors from 'cors'
 
 
 app.use(cors())
 
-app.use(json())
-app.use(text())
-
-
 class CivitAiResponse {
   constructor({ response, url }) { 
     if (response) {
       this.headers = response.headers   
     }
-    this.url = url
+    if (url) {
+      this.url = url 
+    }
+   
 
   }
   getFilename() {
@@ -35,19 +34,49 @@ class CivitAiResponse {
   }
 }
 
+
 app.get('/civitai/getDetails', (req, res) => {
-    try {
-      (async function () { 
-        const url = req.query.url
-        const response = await fetch(url)      
-        const filename = new CivitAiResponse({response, url}).getFilename()
-        const fileCode = new CivitAiResponse({ url }).getFileCode()
-        
-        res.send({filename, fileCode}) 
-      })()
-    } catch (error) {
-      console.error(error)
-    }          
-  ;
+  try {
+      const params = req.query;
+      if (Object.keys(params).length === 0 && params.constructor === Object) {
+        throw new Error(JSON.stringify({
+          name:'invalid url parameter',
+          message: "url Parameters must be 'url'"
+        })) 
+      
+      }
+
+      const newUrl = new URL(params.url)
+    const href = newUrl.href
+    
+      if (newUrl.hostname !== 'civitai.com') { 
+        throw new Error(JSON.stringify({
+          name:'invalid url hostname',
+          message: "only accept 'civitai.com'"
+        })) 
+    }
+    
+    (async function (endp) {    
+      const response = await fetch(endp)       
+      const filename = new CivitAiResponse({response}).getFilename()
+      const fileCode = new CivitAiResponse({ url:endp }).getFileCode()
+      
+      res.send({filename, fileCode}) 
+    })(href)
+
+  } catch (err) {
+
+    const error = JSON.parse(err.message)
+    console.log(err.message)
+    res.send(error)
+
+  }
+            
+  
 })
 app.listen(process.env.PORT || 3000)
+
+
+// const url = new URL('https://civitai.com/api/download/models/19084')
+
+// console.log(url.pathname)
